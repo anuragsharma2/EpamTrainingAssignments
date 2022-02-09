a3a5df44bdfbaef0a1c079574f5d65c15d22f51e
Anurag Sharma2 <anurag_sharma2@epam.com>
2022-02-09 17:24:44 +0530

    angular

--- assignment-app/src/app/app.component.ts
+++ assignment-app/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { AbstractEmitterVisitor } from '@angular/compiler/src/output/abstract_emitter';
 import { Component } from '@angular/core';
 
 @Component({
@@ -23,10 +24,51 @@ export class AppComponent {
     if(pno.length!=10)
       alert("Invalid phone number, please enter phone number with 10 digits")
   }
-  validateLogin(uid:string,pwd:string){
-    if(uid=="Anurag" && pwd=="123")
-      alert("Valid User");
-    else
-      alert("Invalid user.  Please enter the correct credentials")
+  
+  strColor="red";
+  strWidth="300";
+  strHeight="300";
+  strFontSize=12;
+  strFontColor="black";
+  users=[
+    { uid:"amit" ,password:"1234"},
+    { uid:"ojas" ,password:"0987"},
+    { uid:"anand" ,password:"12345"}
+  ]
+  displayTable=false;
+  bLogin=true;
+  bRegister=false;
+  changeToLogin(){
+    this.bLogin=true;
+    this.bRegister=false;
+  }
+  changeToRegister(){
+    this.bLogin=false;
+    this.bRegister=true;
+  }
+  login(uid:string,password:string){
+    for(let user of this.users){
+      if(user.uid==uid && user.password==password){
+        this.displayTable=true;
+        return ;
+      }
+      alert("Invalid UserID and Password ")
+    }
+  }
+  user= { uid:"" ,password:""}
+  makeObj(uid:string,password:string):any{
+    this.user.uid=uid;
+    this.user.password=password;
+    return this.user;
+  }
+  register(uid:string,password:string){
+    this.makeObj(uid,password);
+    this.users.push(this.user);
+  }
+  deleteUser(index:number){
+    this.users.splice(index,1);
+  }
+  editUser(index:number){
+    
   }
 }
